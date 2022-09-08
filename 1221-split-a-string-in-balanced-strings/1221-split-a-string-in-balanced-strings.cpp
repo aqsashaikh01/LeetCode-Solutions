5e73767194320c3dcabb8251d8dcb100f381cc85
Aqsa Shaikh <81984142+aqsashaikh01@users.noreply.github.com>
2022-09-08 10:03:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (39.77%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int bal=0,count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='L')
+                bal++;
+            else
+                bal--;
+            
+            if(bal==0)
+                count++;
+        }
+        return count;      
+    }
+};
